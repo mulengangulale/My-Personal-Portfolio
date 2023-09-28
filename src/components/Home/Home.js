2fdcb823d6910c2ff5eb1c747f755cb921673054
mulengangulale <ngulalemulenga@gmail.com>
2023-09-27 23:41:09 -0400

    Created the sidebar navigation. More details to be added to it

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./Home.css"
+
+function Home(){
+    return (
+        <div>Home Section Here</div>
+    )
+}
+
+export default Home;
---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,30 @@
+import React, {useState} from "react";
+import "./Sidebar.css"
+import Home from "../Home/Home";
+import { BsChevronLeft, BsChevronRight } from "react-icons/bs";
+
+function Sidebar(){
+
+    const [expandSidebar, setExpandSidebar] = useState(true)
+    function handleExpandClick (){
+        setExpandSidebar(!expandSidebar)
+    }
+    return(
+        <div className="container-fluid sidebar-section">
+            <div className={expandSidebar ? "sidebar-expand sidebar": "sidebar"}> 
+                <div className="icon-for-sidebar-expand-and-collapse">
+                    <p onClick={handleExpandClick}>
+                        {expandSidebar ? <BsChevronLeft size={30}/> : <BsChevronRight size={30}/>}
+                    </p>
+                </div>
+            </div>
+
+            <div className="container">
+                <Home />
+            </div>
+
+        </div>
+    )
+}
+
+export default Sidebar;
