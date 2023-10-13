fb0fc2154c53cd82091c60fe0e6bc1359cf978bc
mulengangulale <ngulalemulenga@gmail.com>
2023-10-13 16:48:12 -0400

    Now working on the Education Section 2

---
+++ src/components/Education/Education.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Education.css"
+
+const Education = () => {
+  return (
+    <div className='education'>
+        <div className="section-title">
+          <h5 className="top-space">Education</h5>
+          <span className="line"></span>
+        </div>
+    </div>
+
+  )
+}
+
+export default Education
