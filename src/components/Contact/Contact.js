e06203154ffccd65d15f1339e4e6a1a2a7c7328b
mulengangulale <ngulalemulenga@gmail.com>
2023-10-20 01:58:36 -0400

    Added the send email implementation

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,8 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "./Contact.css"
 import {BsSend} from "react-icons/bs"
 
 const Contact = () => {
+    const API = "http://localhost:8080/sendemail";
+
+    const [name, setName]  = useState();
+    const [email, setEmail]  = useState();
+    const [jobtype, setJobType]  = useState();
+    const [message, setMessage]  = useState();
+
+    const sendEmail = () =>{
+        fetch(API, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                Accept: "application.json"
+            },
+            body: JSON.stringify({
+                name,
+                email,
+                jobtype,
+                message
+            })
+        })
+        .then(res=>res.json())
+        .then((result) => {
+            if(result.error){
+              
+            }
+            setName("");
+            setEmail("");
+            setJobType("");
+            setMessage("");
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
+
   return (
     <div className='container contact-section' id="contact">
         <div className="section-title contact-heading">
@@ -26,16 +62,25 @@ const Contact = () => {
                     <form>
                         <div className='Contact-form'>
                             <label className='label-form'>Name</label>
-                            <input type='text' className='form-control'/>
+                            <input type='text' className='form-control'
+                                value={name}
+                                onChange={(e)=>setName(e.target.value)}
+                            />
 
                         </div>
                         <div className='Contact-form'>
                             <label className='label-form'>Email</label>
-                            <input type='text' className='form-control'/>
+                            <input type='text' className='form-control'
+                                value={email}
+                                onChange={(e)=>setEmail(e.target.value)}
+                            />
                         </div>
                         <div className='Contact-form'>
                             <label className='label-form'>Job Types</label> <br></br>
-                            <select className='custom-select'>
+                            <select className='custom-select'
+                                value={jobtype}
+                                onChange={(e)=>setJobType(e.target.value)}
+                            >
                                 <option>Recruiter</option>
                                 <option>Manager</option>
                                 <option>Full-Time</option>
@@ -46,9 +91,12 @@ const Contact = () => {
                         </div>
                         <div className='Contact-form'>
                             <label className='label-form'>Message</label>
-                            <textarea rows={4} type='text' className='form-control'/>
+                            <textarea rows={4} type='text' className='form-control'
+                                value={message}
+                                onChange={(e)=>setMessage(e.target.value)}
+                            />
                         </div>
-                        <div className='button-submit' >
+                        <div className='button-submit' onClick={sendEmail}>
                             <div>Send <BsSend scale={20}/></div>
                         </div>
                     </form>
