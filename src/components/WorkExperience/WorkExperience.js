a2624b1912ffbd07e662c83531c9d488a8a53fa1
mulengangulale <ngulalemulenga@gmail.com>
2023-10-22 15:09:52 -0400

    Done editing the work experience section

--- src/components/WorkExperience/WorkExperience.js
+++ src/components/WorkExperience/WorkExperience.js
@@ -17,7 +17,7 @@ const WorkExperience = () => {
     {
       companyname: "EcoCAR Competion",
       position: "Software Engineering Intern",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      des: "The EcoCAR EV Challenge is a four-year collegiate automotive engineering competition, providing an unparalleled, hands-on educational experience that transforms the traditional classroom environment into a hub of automotive innovation",
       period: "Aug 2023 - Present",
       techskills: [
         {
@@ -34,7 +34,7 @@ const WorkExperience = () => {
     {
       companyname: "Uber",
       position: "Software Engineering Intern",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      des: "During my tenure at Uber, I spearheaded three key projects. First, I designed a robust Command Line Interface (CLI) in GO and Python to schedule and manage Machine Learning Orchestration pipelines, enhancing user control and task execution. Second, I engineered an automated Python solution to synchronize tier information between Machine Learning Explorer Projects and pipeline configurations, reducing inconsistencies through daily scheduled synchronization. Lastly, I led the implementation of ISO time duration standards using GO, enabling users to specify execution dates in days and promoting greater temporal flexibility in Machine Learning pipelines. These projects showcased my technical skills and problem-solving abilities.",
       period: "May 2023 - Aug 2023",
       techskills: [
         {
@@ -51,7 +51,7 @@ const WorkExperience = () => {
     {
       companyname: "ServiceNow",
       position: "Quality Engineering Intern",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      des: "During my tenure at ServiceNow, I achieved significant advancements in database automation. I developed Python scripts, elevating automation efficiency by 12% and replacing manual data entry in Excel spreadsheets. Collaborating with peers, I engineered a groundbreaking comparison tool using UI Builder, Node.JS, and React, shifting test execution monitoring from manual Excel sheets to automation, yielding a 17% efficiency increase. I introduced automated testing frameworks, resulting in a 20% improvement in code product efficiency. Additionally, I played a pivotal role in end-to-end product development, spanning testing, deployment, and comprehensive reporting on performance, quality, security, and stability, collaborating with the software development team.",
       period: "May 2022 - Aug 2022",
       techskills: [
         {
@@ -68,7 +68,7 @@ const WorkExperience = () => {
     {
       companyname: "Apple",
       position: "Apple Project Researcher",
-      des: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      des: "During my involvement in the Apple Coding Project, I played a key role in several aspects. I was responsible for the development and creation of the project's web-based educational platform. Additionally, I provided guidance and instruction to middle school students, teaching them how to code and build apps using Swift Playground Apps. I also contributed to the completion of instructional designs for the Apple Coding Project and actively promoted its educational objectives. Furthermore, I collaborated with students, faculty, and staff from other institutions involved in the project to aid in the development of research products, including manuscripts, articles, and reports.",
       period: "Nov 2022 - May 2023",
       techskills: [
         {
